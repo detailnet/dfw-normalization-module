df111e726192058f4b9d4b25bfefb8e4521e5c75
Ivan Wolf <iwolf@detailag.ch>
2018-06-06 14:01:10 +0200

    Formatted config

--- config/module.config.php
+++ config/module.config.php
@@ -5,23 +5,34 @@ return [
         'abstract_factories' => [
         ],
         'aliases' => [
-            'jms_serializer.php_serialization_visitor' => Detail\Normalization\JMSSerializer\PhpSerializationVisitor::CLASS,
-            'jms_serializer.php_deserialization_visitor' => Detail\Normalization\JMSSerializer\PhpDeserializationVisitor::CLASS,
+            'jms_serializer.php_serialization_visitor' =>
+                Detail\Normalization\JMSSerializer\PhpSerializationVisitor::CLASS,
+            'jms_serializer.php_deserialization_visitor' =>
+                Detail\Normalization\JMSSerializer\PhpDeserializationVisitor::CLASS,
         ],
         'invokables' => [
-            Detail\Normalization\JMSSerializer\EventDispatcher\Subscriber\DoctrineProxySubscriber::CLASS => Detail\Normalization\JMSSerializer\EventDispatcher\Subscriber\DoctrineProxySubscriber::CLASS,
-            Detail\Normalization\JMSSerializer\Handler\ArrayCollectionHandler::CLASS => Detail\Normalization\JMSSerializer\Handler\ArrayCollectionHandler::CLASS,
-            Detail\Normalization\JMSSerializer\Handler\DateHandler::CLASS => Detail\Normalization\JMSSerializer\Handler\DateHandler::CLASS,
-            Detail\Normalization\JMSSerializer\Handler\DateImmutableHandler::CLASS => Detail\Normalization\JMSSerializer\Handler\DateImmutableHandler::CLASS,
+            Detail\Normalization\JMSSerializer\EventDispatcher\Subscriber\DoctrineProxySubscriber::CLASS =>
+                Detail\Normalization\JMSSerializer\EventDispatcher\Subscriber\DoctrineProxySubscriber::CLASS,
+            Detail\Normalization\JMSSerializer\Handler\ArrayCollectionHandler::CLASS =>
+                Detail\Normalization\JMSSerializer\Handler\ArrayCollectionHandler::CLASS,
+            Detail\Normalization\JMSSerializer\Handler\DateHandler::CLASS =>
+                Detail\Normalization\JMSSerializer\Handler\DateHandler::CLASS,
+            Detail\Normalization\JMSSerializer\Handler\DateImmutableHandler::CLASS =>
+                Detail\Normalization\JMSSerializer\Handler\DateImmutableHandler::CLASS,
 
             // Add our own version of the default subscriber to support HalCollection types
-            'jms_serializer.doctrine_proxy_subscriber' => Detail\Normalization\JMSSerializer\EventDispatcher\Subscriber\DoctrineProxySubscriber::CLASS,
+            'jms_serializer.doctrine_proxy_subscriber' =>
+                Detail\Normalization\JMSSerializer\EventDispatcher\Subscriber\DoctrineProxySubscriber::CLASS,
         ],
         'factories' => [
-            Detail\Normalization\JMSSerializer\PhpSerializationVisitor::CLASS => Detail\Normalization\Factory\JMSSerializer\PhpSerializationVisitorFactory::CLASS,
-            Detail\Normalization\JMSSerializer\PhpDeserializationVisitor::CLASS => Detail\Normalization\Factory\JMSSerializer\PhpDeserializationVisitorFactory::CLASS,
-            Detail\Normalization\Normalizer\JMSSerializerBasedNormalizer::CLASS => Detail\Normalization\Factory\Normalizer\JMSSerializerBasedNormalizerFactory::CLASS,
-            Detail\Normalization\Options\ModuleOptions::CLASS => Detail\Normalization\Factory\Options\ModuleOptionsFactory::CLASS,
+            Detail\Normalization\JMSSerializer\PhpSerializationVisitor::CLASS =>
+                Detail\Normalization\Factory\JMSSerializer\PhpSerializationVisitorFactory::CLASS,
+            Detail\Normalization\JMSSerializer\PhpDeserializationVisitor::CLASS =>
+                Detail\Normalization\Factory\JMSSerializer\PhpDeserializationVisitorFactory::CLASS,
+            Detail\Normalization\Normalizer\JMSSerializerBasedNormalizer::CLASS =>
+                Detail\Normalization\Factory\Normalizer\JMSSerializerBasedNormalizerFactory::CLASS,
+            Detail\Normalization\Options\ModuleOptions::CLASS =>
+                Detail\Normalization\Factory\Options\ModuleOptionsFactory::CLASS,
         ],
         'initializers' => [
             Detail\Normalization\Normalizer\NormalizerInitializer::CLASS,

cc92185eb126e5461d97bf2ffc240802185754df
Ivan Wolf <iwolf@detailag.ch>
2018-06-06 15:31:47 +0200

    Added tests for factories

---
+++ tests/Normalizer/NormalizerInitializerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace DetailTest\Normalization\Factory;
+
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+abstract class FactoryTestCase extends TestCase
+{
+    /**
+     * @var MockObject
+     */
+    private $services;
+
+    protected function setUp()
+    {
+        $this->services = $this->getMockBuilder(ServiceLocatorInterface::class)->getMock();
+    }
+
+    protected function getServices(): MockObject
+    {
+        return $this->services;
+    }
+}
